ed26e913dd1189786f07172abce93a5131125d10
Troy Kelly <tkelly@aftrs.edu.au>
2022-04-02 02:00:33 +0000

    Markdown lint fixing

--- .mdl.rb
+++ .mdl.rb
@@ -5,7 +5,13 @@ all
 rule 'MD013', :line_length => 99999
 
 # Allow in-line HTML
+exclude_rule 'MD002'
+exclude_rule 'MD007'
+exclude_rule 'MD029'
+exclude_rule 'MD031'
 exclude_rule 'MD033'
+exclude_rule 'MD036'
+exclude_rule 'MD041'
 
 # Nested lists should be indented with four spaces.
 rule 'MD007', :indent => 4
