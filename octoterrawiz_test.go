66493c9ae7f69fe243b2e96def2260888ee4ec98
Matthew Casperson <matthew.casperson@gmail.com>
2024-07-25 17:13:54 +1000

    test repeat spread operations

--- octoterrawiz_test.go
+++ octoterrawiz_test.go
@@ -57,50 +57,53 @@ func TestSpreadVariables(t *testing.T) {
 			}},
 		}
 
-		if err := step.Execute(); err != nil {
-			t.Fatalf("Error executing step: %v", err)
-		}
+		// we must be able to repeat this step with no changes
+		for i := 0; i < 3; i++ {
+			if err := step.Execute(); err != nil {
+				t.Fatalf("Error executing step: %v", err)
+			}
 
-		lvs, err := newSpaceClient.LibraryVariableSets.GetAll()
+			lvs, err := newSpaceClient.LibraryVariableSets.GetAll()
 
-		if err != nil {
-			t.Fatalf("Error getting library variable sets: %v", err)
-		}
+			if err != nil {
+				t.Fatalf("Error getting library variable sets: %v", err)
+			}
 
-		lvsVariable, err := variables.GetVariableSet(newSpaceClient, newSpaceClient.GetSpaceID(), lvs[0].VariableSetID)
+			lvsVariable, err := variables.GetVariableSet(newSpaceClient, newSpaceClient.GetSpaceID(), lvs[0].VariableSetID)
 
-		if err != nil {
-			t.Fatalf("Error getting library variable sets: %v", err)
-		}
+			if err != nil {
+				t.Fatalf("Error getting library variable sets: %v", err)
+			}
 
-		if len(lvsVariable.Variables) != 7 {
-			t.Fatalf("Expected 7 variables, got %v", len(lvsVariable.Variables))
-		}
+			if len(lvsVariable.Variables) != 7 {
+				t.Fatalf("Expected 7 variables, got %v", len(lvsVariable.Variables))
+			}
 
-		// There must be 4 sensitive variables, and they must all be unscoped
-		sensitiveVariables := lo.Filter(lvsVariable.Variables, func(item *variables.Variable, index int) bool {
-			return item.IsSensitive &&
-				len(item.Scope.Environments) == 0 &&
-				len(item.Scope.Roles) == 0 &&
-				len(item.Scope.Machines) == 0 &&
-				len(item.Scope.Actions) == 0 &&
-				len(item.Scope.TenantTags) == 0 &&
-				len(item.Scope.ProcessOwners) == 0 &&
-				len(item.Scope.Channels) == 0
-		})
+			// There must be 4 sensitive variables, and they must all be unscoped
+			sensitiveVariables := lo.Filter(lvsVariable.Variables, func(item *variables.Variable, index int) bool {
+				return item.IsSensitive &&
+					len(item.Scope.Environments) == 0 &&
+					len(item.Scope.Roles) == 0 &&
+					len(item.Scope.Machines) == 0 &&
+					len(item.Scope.Actions) == 0 &&
+					len(item.Scope.TenantTags) == 0 &&
+					len(item.Scope.ProcessOwners) == 0 &&
+					len(item.Scope.Channels) == 0
+			})
 
-		if len(sensitiveVariables) != 4 {
-			t.Fatalf("Expected 4 variables, got %v", len(sensitiveVariables))
-		}
+			if len(sensitiveVariables) != 4 {
+				t.Fatalf("Expected 4 variables, got %v", len(sensitiveVariables))
+			}
 
-		// The three sensitive variables that shared a name must now have 3 regular variables each scoped
-		// to an environment
-		originalVariables := lo.Filter(lvsVariable.Variables, func(item *variables.Variable, index int) bool {
-			return item.Name == "Test.SecretVariable" && !item.IsSensitive && len(item.Scope.Environments) == 1
-		})
+			// The three sensitive variables that shared a name must now have 3 regular variables each scoped
+			// to an environment
+			originalVariables := lo.Filter(lvsVariable.Variables, func(item *variables.Variable, index int) bool {
+				return item.Name == "Test.SecretVariable" && !item.IsSensitive && len(item.Scope.Environments) == 1
+			})
 
-		if len(originalVariables) != 3 {
-			t.Fatalf("Expected 3 variables, got %v", len(originalVariables))
+			if len(originalVariables) != 3 {
+				t.Fatalf("Expected 3 variables, got %v", len(originalVariables))
+			}
 		}
 
 		return nil
