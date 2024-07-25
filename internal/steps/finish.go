629168d8251afa7a09a40362df3c66578ab23861
Matthew Casperson <matthew.casperson@gmail.com>
2024-07-25 14:13:37 +1000

    noted CaC settings must be recreated

--- internal/steps/finish.go
+++ internal/steps/finish.go
@@ -20,6 +20,7 @@ func (s FinishStep) GetContainer(parent fyne.Window) *fyne.Container {
 		The migration tool has now completed.
 		Note that there are resources and settings that can not be migrated.
 		Where possible, these resources can be manually recreated or updated in the destination space:
+		* Config-as-Code settings
 		* Certificates
 		* Account, feed, Git, ServiceNow, Jira, Sumo, and Slunk credentials
 		* Sensitive variables defined directly in a step, for example in a step template that has a sensitive parameter, or
