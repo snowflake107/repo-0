b51eb0f5ade8c55acd62716a202e4130e37f52d7
Matthew Casperson <matthewcasperson@gmail.com>
2024-08-22 07:26:09 +1000

    Streamline the terraform testing process

--- internal/steps/test_terraform.go
+++ internal/steps/test_terraform.go
@@ -14,45 +14,55 @@ import (
 type TestTerraformStep struct {
 	BaseStep
 	Wizard wizard.Wizard
+	result *widget.Label
 }
 
 func (s TestTerraformStep) GetContainer(parent fyne.Window) *fyne.Container {
 
-	bottom, _, next := s.BuildNavigation(func() {
+	bottom, _, _ := s.BuildNavigation(func() {
 		s.Wizard.ShowWizardStep(WelcomeStep{
 			Wizard:   s.Wizard,
 			BaseStep: BaseStep{State: s.State}})
 	}, func() {
-		s.Wizard.ShowWizardStep(OctopusDetails{
-			Wizard:   s.Wizard,
-			BaseStep: BaseStep{State: s.State}})
+		s.result.SetText("🔵 Testing Terraform installation.")
+
+		if s.Execute() {
+			s.Wizard.ShowWizardStep(OctopusDetails{
+				Wizard:   s.Wizard,
+				BaseStep: BaseStep{State: s.State}})
+		} else {
+			s.notInstalled()
+		}
 	})
-	next.Disable()
 
 	heading := widget.NewLabel("Test Terraform")
 	heading.TextStyle = fyne.TextStyle{Bold: true}
 
 	label1 := widget.NewLabel(strutil.TrimMultilineWhitespace(`
 		You must have Terraform installed to use this tool.
-		Click the "Test" button to check if Terraform is installed.
+		Click the "Next" button to check if Terraform is installed.
 	`))
 
 	linkUrl, _ := url.Parse("https://developer.hashicorp.com/terraform/install")
 	link := widget.NewHyperlink("Learn how to install Terraform.", linkUrl)
 
-	result := widget.NewLabel("")
-	testTerraform := widget.NewButton("Test Terraform is Installed", func() {
-		cmd := exec.Command("terraform", "-version")
-		if err := cmd.Run(); err != nil {
-			result.SetText("🔴 Terraform does not appear to be installed. You must install Terraform before proceeding.")
-		} else {
-			result.SetText("🟢 Terraform is installed. Click the Next button to proceed.")
-			next.Enable()
-		}
-	})
-	middle := container.New(layout.NewVBoxLayout(), heading, label1, link, testTerraform, result)
+	s.result = widget.NewLabel("")
+
+	middle := container.New(layout.NewVBoxLayout(), heading, label1, link, s.result)
 
 	content := container.NewBorder(nil, bottom, nil, nil, middle)
 
 	return content
+}
+
+func (s TestTerraformStep) notInstalled() {
+	s.result.SetText("🔴 Terraform does not appear to be installed. You must install Terraform before proceeding.")
+}
+
+func (s TestTerraformStep) Execute() bool {
+	cmd := exec.Command("terraform", "-version")
+	if err := cmd.Run(); err != nil {
+		return false
+	}
+	return true
 }
